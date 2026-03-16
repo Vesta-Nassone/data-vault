df5569fc3b67bb092e435ece975eecae23683a2b
Vesta-Nassone <vestanassone@gmail.com>
2026-03-16 17:14:05 +0200

    Add the base endpoints.

--- data_vault/urls.py
+++ data_vault/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls)
+    path("admin/", admin.site.urls),
+    path("api/", include("documents.urls_api")),
+    path("", include("documents.urls_ui")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
